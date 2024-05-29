0c391519cc74dd1f55e0cc3f310d4ba99a967c02
TaeYoung1217 <wkdxodud44@gmail.com>
2024-05-29 15:48:27 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
@@ -8,6 +10,13 @@ const firebaseConfig = {
   // The value of `databaseURL` depends on the location of the database
   databaseURL:
     "https://carrot-market-76774-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  storageBucket: "gs://carrot-market-76774.appspot.com",
+  apiKey: "AIzaSyAlLNlvEKXEdveugn-B8Snn72dinPMzBFE",
+  authDomain: "carrot-market-76774.firebaseapp.com",
+  projectId: "carrot-market-76774",
+  messagingSenderId: "635040660058",
+  appId: "1:635040660058:web:40fb193546e139a9ddf663",
+  measurementId: "G-WFM8RN1RRG",
 };
 
 // Initialize Firebase
@@ -15,3 +24,5 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Realtime Database and get a reference to the service
 const database = getDatabase(app);
+const storage = getStorage(app);
+const auth = getAuth(app);
